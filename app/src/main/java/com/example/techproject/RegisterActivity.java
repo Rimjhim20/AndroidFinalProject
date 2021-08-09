95f91814c50dd12f1ab11ce11615c4e9385e98bd
Rimjhim <kumaririmjhim720@gmail.com>
2021-08-09 14:50:14 +0530

    Android Training final project-Newzy App

--- app/src/main/java/com/example/techproject/RegisterActivity.java
+++ app/src/main/java/com/example/techproject/RegisterActivity.java
@@ -1,10 +1,7 @@
 package com.example.techproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,77 +9,196 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity implements  View.OnClickListener{
-EditText name,email,pass,cpass;
-    FirebaseAuth mFirebaseAuth;
+public class RegisterActivity extends AppCompatActivity {
+    DatabaseHelper db;
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        db = new DatabaseHelper(this);
+        mTextUsername = findViewById(R.id.edittext_username);
+        mTextPassword = findViewById(R.id.edittext_password);
+        mTextCnfPassword = findViewById(R.id.edittext_cnf_password);
+        mButtonRegister = findViewById(R.id.button_register);
+        mTextViewLogin =findViewById(R.id.textview_login);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent LoginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(LoginIntent);
+            }
+        });
 
-        name=findViewById(R.id.loginpass);
-        email=findViewById(R.id.regemail);
-        pass=findViewById(R.id.regpass);
-        cpass=findViewById(R.id.regcpass);
-        Button button=findViewById(R.id.button);
-        button.setOnClickListener(this);
-        TextView btn=findViewById(R.id.alreadyaccount);
-         btn.setOnClickListener(this);
-        mFirebaseAuth=FirebaseAuth.getInstance();
-   }
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                String cnf_pwd = mTextCnfPassword.getText().toString().trim();
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View v) {
 
-        String Personname = name.getText().toString();
-        String Personemail = email.getText().toString();
-        String pwd = pass.getText().toString();
-        String cpwd = cpass.getText().toString();
+                if(pwd.equals(cnf_pwd)){
+                    long val = db.addUser(user,pwd);
+                    if(val > 0){
 
-        if (Personname.isEmpty()) {
-            name.setError("Please Enter email id");
-            name.requestFocus();
-        }
-         else if (Personemail.isEmpty()) {
-            email.setError("Please enter username");
-            email.requestFocus();
-        } else if (pwd.isEmpty()) {
-            pass.setError("Please enter password");
-            pass.requestFocus();
-        } else if (cpwd.isEmpty()) {
-            cpass.setError("Please enter confirm password");
-            cpass.requestFocus();
-        }else if(!pwd.equals(cpwd)){
-            Toast.makeText(this, "Invalid password", Toast.LENGTH_SHORT).show();
-        } else if (!(Personemail.isEmpty() && pwd.isEmpty())) {
-            mFirebaseAuth.createUserWithEmailAndPassword(Personemail, pwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (user.isEmpty()) {
+                            mTextUsername.setError("Please Enter username ");
+                            mTextUsername.requestFocus();
 
-                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+
+                        }
+                        else if( pwd.isEmpty()){
+                            mTextPassword.setError("Please Enter Password ");
+                            mTextPassword.requestFocus();
+                        }
+                        else if( cnf_pwd.isEmpty()){
+                            mTextCnfPassword.setError("Please Enter Confirm Password ");
+                            mTextCnfPassword.requestFocus();
+                        }
+                        else {
+                            Toast.makeText(RegisterActivity.this, "You have registered", Toast.LENGTH_SHORT).show();
+                            Intent moveToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(moveToLogin);
+                        }
+                    }
+                    else{
+                        Toast.makeText(RegisterActivity.this,"Registeration Error",Toast.LENGTH_SHORT).show();
+                    }
 
                 }
-            });
-        } else {
-            Toast.makeText(this, "Error Occurred", Toast.LENGTH_SHORT).show();
-        }
-
-    
-        switch(v.getId()){
-            case R.id.alreadyaccount:
-                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
-                break;
-        }
-
+                else{
+                    Toast.makeText(RegisterActivity.this,"Password is not matching",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-
-
 }
+
+
+
+//package com.example.techproject;
+//
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.annotation.SuppressLint;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.RadioButton;
+//import android.widget.Spinner;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//
+//
+//import java.util.ArrayList;
+//
+//public class RegisterActivity extends AppCompatActivity implements  View.OnClickListener{
+//EditText name,email,pass,cpass;
+//    EditText edt,edt1,edt2,edt3,edt4,edt5;
+//    RadioButton rdo1,rdo2;
+//    Spinner spinner;
+//    Helper DB;
+//    ArrayList qualification;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_register);
+//        name=findViewById(R.id.loginpass);
+//        email=findViewById(R.id.regname);
+//        pass=findViewById(R.id.regpass);
+//        cpass=findViewById(R.id.regcpass);
+//        DB=new Helper (this);
+//
+//        Button button=findViewById(R.id.button);
+//        button.setOnClickListener(this);
+//        TextView btn=findViewById(R.id.alreadyaccount);
+//         btn.setOnClickListener(this);
+//        spinner=findViewById(R.id.spinner);
+//        qualification=new ArrayList();
+//        qualification.add("not specified");
+//        qualification.add("Undergraduate");
+//        qualification.add("Postgraduate");
+//        qualification.add("Masters");
+//        qualification.add("Phd");
+//        ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,qualification);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+//        spinner.setAdapter(adapter);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+//            }
+//        });
+//   }
+//
+//    @SuppressLint("NonConstantResourceId")
+//    @Override
+//    public void onClick(View v) {
+//
+//        String Personname = name.getText().toString();
+//        String Personemail = email.getText().toString();
+//        String pwd = pass.getText().toString();
+//        String cpwd = cpass.getText().toString();
+//
+//        if (Personname.isEmpty()) {
+//            name.setError("Please Enter email id");
+//            name.requestFocus();
+//        }
+//         else if (Personemail.isEmpty()) {
+//            email.setError("Please enter username");
+//            email.requestFocus();
+//        } else if (pwd.isEmpty()) {
+//            pass.setError("Please enter password");
+//            pass.requestFocus();
+//        } else if (cpwd.isEmpty()) {
+//            cpass.setError("Please enter confirm password");
+//            cpass.requestFocus();
+//        }
+//        else if (cpwd.isEmpty()) {
+//            cpass.setError("Please enter confirm password");
+//            cpass.requestFocus();
+//        }
+//        else if(!pwd.equals(cpwd)){
+//            Toast.makeText(this, "Invalid password", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (Personemail.equals(" ")|| Personname.equals(" ") || pwd.equals(" ")|| cpwd.equals(" ")) {
+//            Toast.makeText(this, "Please Enter all fields", Toast.LENGTH_SHORT).show();
+//        }else {
+//            if(pwd.equals(cpwd)){
+//                Boolean checkuser = DB.checkusername(Personname);
+//                if(checkuser==false){
+//                    Boolean insert = DB.insertData(Personname,pwd);
+//                    if(insert==true){
+//                        Toast.makeText(this, "Registered successfully", Toast.LENGTH_SHORT).show();
+//                        Intent intent= new Intent( getApplicationContext(), LoginActivity.class);
+//                        startActivity(intent);
+//                    }else{
+//                        Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show();
+//                    }
+//                }else{
+//                    Toast.makeText(this, "User Already exists! Please Sign in", Toast.LENGTH_SHORT).show();
+//                }
+//            }else{
+//                Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show();
+//            }
+//
+//        }
+//
+//    }
+//
+//
+//}
--- app/src/main/java/com/example/techproject/option.java
+++ app/src/main/java/com/example/techproject/option.java
@@ -6,23 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class option extends AppCompatActivity {
+public class option extends AppCompatActivity implements  View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
         Button news=findViewById(R.id.news);
-        news.setOnClickListener(new View.OnClickListener(){
+        news.setOnClickListener(this);
+        Button weather=findViewById(R.id.weather);
+        weather.setOnClickListener(this);
+        }
+    public void onClick(View v){
 
+        switch(v.getId()){
+            case R.id.weather:
+                startActivity(new Intent(option.this, Weather.class));
+                break;
+            case R.id.news:
+                startActivity(new Intent(option.this, MainActivity.class));
+                break;
 
-
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(option.this,MainActivity.class));
-            }
-        });
+        }
     }
-}
+    }
